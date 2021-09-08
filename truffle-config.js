dc0d4516a0e19a88a2984435234ec802e1b8d12e
devdattabidkar <67537580+devdattabidkar@users.noreply.github.com>
2021-09-08 16:47:09 +0530

    Hi

---
+++ truffle-config.js
@@ -0,0 +1,35 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+require('dotenv').config()
+
+module.exports = {
+  networks: {
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(process.env.MNEMONIC, process.env.RINKEBY_RPC_URL)
+      },
+      network_id: "4",
+      networkCheckTimeout: 1000000,
+      timeoutBlocks: 200,
+      addressIndex: 2
+
+    },
+    mainnet: {
+      provider: () => {
+        return new HDWalletProvider(process.env.MAINNET_MNEMONIC, process.env.MAINNET_RPC_URL)
+      },
+      network_id: '1',
+      skipDryRun: true,
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.8.0',
+    },
+  },
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ]
+}
